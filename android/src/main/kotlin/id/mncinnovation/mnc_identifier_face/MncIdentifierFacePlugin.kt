158cb1b21fef3309333e3cc7769442451f980db7
hussienhamdy <hussienhamdy100@gmail.com>
2024-01-09 17:15:18 +0200

    fix to import Random class

--- android/src/main/kotlin/id/mncinnovation/mnc_identifier_face/MncIdentifierFacePlugin.kt
+++ android/src/main/kotlin/id/mncinnovation/mnc_identifier_face/MncIdentifierFacePlugin.kt
@@ -18,6 +18,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import kotlin.random.Random
 
 /** MncIdentifierFacePlugin */
 class MncIdentifierFacePlugin: FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.ActivityResultListener {
