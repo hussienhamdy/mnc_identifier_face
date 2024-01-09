ae1c0e9ae1bd8c4410be6accfd66d1a70b998443
hussienhamdy <hussienhamdy100@gmail.com>
2024-01-09 17:11:54 +0200

    fix to face detection moves initialization

--- android/src/main/kotlin/id/mncinnovation/mnc_identifier_face/MncIdentifierFacePlugin.kt
+++ android/src/main/kotlin/id/mncinnovation/mnc_identifier_face/MncIdentifierFacePlugin.kt
@@ -44,9 +44,32 @@ class MncIdentifierFacePlugin: FlutterPlugin, MethodCallHandler, ActivityAware, 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     this.result = result
     if (call.method == "startLivenessDetection") {
-      //set 0 to disable low memory allocation warning popup
-      val lowMemoryThreshold = 0
-      MNCIdentifier.setLowMemoryThreshold(lowMemoryThreshold)
+      val detectionModes = mutableListOf(
+        DetectionMode.HOLD_STILL
+      )
+      var random = 0;
+      for (i in 1 until 3) {
+        random = Random.nextInt(4);
+        Log.d("tag","random random $random");
+        when (random) {
+          0 -> {
+            detectionModes.add(i, DetectionMode.TURN_RIGHT)
+          }
+          1 -> {
+            detectionModes.add(i, DetectionMode.TURN_LEFT)
+          }
+          2 -> {
+            detectionModes.add(i, DetectionMode.SMILE)
+          }
+          else -> {
+            detectionModes.add(i, DetectionMode.BLINK)
+          }
+        }
+      }
+      MNCIdentifier.setDetectionModeSequence(
+        true, detectionModes
+      );
+      MNCIdentifier.setLowMemoryThreshold(50) // for face detection
       activity.startActivityForResult(MNCIdentifier.getLivenessIntent(context), LIVENESS_DETECTION_REQUEST_CODE)
     } else {
       result.notImplemented()
@@ -62,9 +85,9 @@ class MncIdentifierFacePlugin: FlutterPlugin, MethodCallHandler, ActivityAware, 
           livenessResult?.let { res ->
             if (res.isSuccess) {  // check if liveness detection success
               // get image result
-              val bitmap = res.getBitmap(context, DetectionMode.SMILE, onError = { message ->
-                result.error("Failed", "Mnc-identifier-face: $message", null)
-              })
+//              val bitmap = res.getBitmap(context, DetectionMode.SMILE, onError = { message ->
+//                result.error("Failed", "Mnc-identifier-face: $message", null)
+//              })
               var gson = Gson()
               result.success(gson.toJson(res))
             } else {  //Liveness Detection Error
